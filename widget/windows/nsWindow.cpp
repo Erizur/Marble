291dbf32986c0e5e3430d7622c48fe57eed9e1c7
AM_Erizur <watchdoukyuusei@outlook.com>
2026-05-16 13:39:59 -0500

    Revert "Bug 1934375 - Take over whole non-client area for PiP windows again, for now. r=win-reviewers,gstoll"
    This reverts commit 39c490cb6d24a650d294eaa7ad7da037d1b1bdd5.

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -668,7 +668,6 @@ nsWindow::nsWindow(bool aIsChildWindow)
     : nsBaseWidget(BorderStyle::Default),
       mFrameState(std::in_place, this),
       mIsChildWindow(aIsChildWindow),
-      mPIPWindow(false),
       mLastPaintEndTime(TimeStamp::Now()),
       mCachedHitTestTime(TimeStamp::Now()),
       mSizeConstraintsScale(GetDefaultScale().scale),
@@ -889,7 +888,6 @@ nsresult nsWindow::Create(nsIWidget* aParent, const LayoutDeviceIntRect& aRect,
       aParent ? (HWND)aParent->GetNativeData(NS_NATIVE_WINDOW) : nullptr;
 
   mIsRTL = aInitData->mRTL;
-  mPIPWindow = aInitData->mPIPWindow;
   mOpeningAnimationSuppressed = aInitData->mIsAnimationSuppressed;
   mAlwaysOnTop = aInitData->mAlwaysOnTop;
   mIsAlert = aInitData->mIsAlert;
@@ -2649,9 +2647,6 @@ bool nsWindow::UpdateNonClientMargins(bool aReflowWindow) {
         }
       }
     }
-  } else if (mPIPWindow &&
-             !StaticPrefs::widget_windows_pip_decorations_enabled()) {
-    metrics.mOffset = metrics.DefaultMargins();
   } else {
     metrics.mOffset = NormalWindowNonClientOffset();
   }
