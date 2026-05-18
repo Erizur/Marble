9de2e7f9f3a6ba6f8efdc2ab3d94646b3ff724e1
AM_Erizur <watchdoukyuusei@outlook.com>
2026-05-18 02:31:47 -0500

    Revert "Bug 1918802 - Simplify scrollbar track drawing. r=mstange,mac-reviewers"
    This reverts commit a13711ff7b5eada2da54659da4c2eb8bfe93bd65.

--- widget/ScrollbarDrawing.h
+++ widget/ScrollbarDrawing.h
@@ -110,14 +110,29 @@ class ScrollbarDrawing {
                                ScrollbarKind, nsIFrame*, const ComputedStyle&,
                                const ElementState&, const DocumentState&,
                                const Colors&, const DPIRatio&);
-  virtual bool PaintScrollbar(DrawTarget&, const LayoutDeviceRect&,
-                              ScrollbarKind, nsIFrame*, const ComputedStyle&,
-                              const ElementState&, const DocumentState&,
-                              const Colors&, const DPIRatio&);
-  virtual bool PaintScrollbar(WebRenderBackendData&, const LayoutDeviceRect&,
-                              ScrollbarKind, nsIFrame*, const ComputedStyle&,
-                              const ElementState&, const DocumentState&,
-                              const Colors&, const DPIRatio&);
+  bool PaintScrollbar(DrawTarget&, const LayoutDeviceRect&, ScrollbarKind,
+                      nsIFrame*, const ComputedStyle&, const ElementState&,
+                      const DocumentState&, const Colors&, const DPIRatio&);
+  bool PaintScrollbar(WebRenderBackendData&, const LayoutDeviceRect&,
+                      ScrollbarKind, nsIFrame*, const ComputedStyle&,
+                      const ElementState&, const DocumentState&, const Colors&,
+                      const DPIRatio&);
+
+  virtual bool PaintScrollbarTrack(DrawTarget&, const LayoutDeviceRect&,
+                                   ScrollbarKind, nsIFrame*,
+                                   const ComputedStyle&, const DocumentState&,
+                                   const Colors&, const DPIRatio&) {
+    // Draw nothing by default. Subclasses can override this.
+    return true;
+  }
+  virtual bool PaintScrollbarTrack(WebRenderBackendData&,
+                                   const LayoutDeviceRect&, ScrollbarKind,
+                                   nsIFrame*, const ComputedStyle&,
+                                   const DocumentState&, const Colors&,
+                                   const DPIRatio&) {
+    // Draw nothing by default. Subclasses can override this.
+    return true;
+  }
 
   template <typename PaintBackendData>
   bool DoPaintDefaultScrollCorner(PaintBackendData&, const LayoutDeviceRect&,
