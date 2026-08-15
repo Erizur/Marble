dc478d79d9f480097af78c808ab9cf56717c22b5
AM_Erizur <watchdoukyuusei@outlook.com>
2026-08-14 22:48:56 -0500

    Initial branding work

--- browser/branding/official/configure.sh
+++ browser/branding/official/configure.sh
@@ -2,4 +2,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME=Firefox
+MOZ_APP_BASENAME=Marble
+MOZ_APP_REMOTINGNAME=com.biidama.marble
+MOZ_DISTRIBUTION_ID=com.biidama.marble
+MOZ_MACBUNDLE_NAME="marble.app"
+MOZ_APP_DISPLAYNAME=Marble
+MOZ_APP_NAME=marble
+
+MOZ_DEVTOOLS=all
+
