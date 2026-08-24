544c3cf00a1999f6abcfa9ecb501b0d85ebac5cc
AM_Erizur <watchdoukyuusei@outlook.com>
2026-08-24 13:21:01 -0500

    Revert "Bug 1967139 - [x11] CSD decoration painting shouldn't honor text scale. r=stransky"
    This reverts commit 437fba298dd6760c439777aa10693ceaa756091a.

--- gfx/src/nsDeviceContext.h
+++ gfx/src/nsDeviceContext.h
@@ -6,7 +6,10 @@
 #define NS_DEVICECONTEXT_H_
 
 #include <stdint.h>                    // for uint32_t
+#include <sys/types.h>                 // for int32_t
 #include "gfxTypes.h"                  // for gfxFloat
+#include "gfxFont.h"                   // for gfxFont::Orientation
+#include "mozilla/Assertions.h"        // for MOZ_ASSERT_HELPER2
 #include "mozilla/RefPtr.h"            // for RefPtr
 #include "nsCOMPtr.h"                  // for nsCOMPtr
 #include "nsCoord.h"                   // for nscoord
@@ -17,7 +20,8 @@
 #include "mozilla/AppUnits.h"          // for AppUnits
 #include "nsFontMetrics.h"             // for nsFontMetrics::Params
 #include "mozilla/gfx/Point.h"         // for IntSize
-#include "mozilla/gfx/PrintPromise.h"  // for PrintEndDocumentPromise
+#include "mozilla/gfx/PrintTarget.h"   // for PrintTarget::PageDoneCallback
+#include "mozilla/gfx/PrintPromise.h"
 
 class gfxContext;
 class gfxTextPerfMetrics;
@@ -40,9 +44,6 @@ enum class ScreenOrientation : uint32_t;
 namespace widget {
 class Screen;
 }  // namespace widget
-namespace gfx {
-class PrintTarget;
-}
 }  // namespace mozilla
 
 class nsDeviceContext final {
