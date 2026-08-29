9445c42d5cf517aba5487bb1e91b0b0ff3b9d827
AM_Erizur <watchdoukyuusei@outlook.com>
2026-08-28 23:02:08 -0500

    Revert "Bug 1858349 - Remove now-unused menuarrow code. r=spohl,mac-reviewers,win-reviewers,rkraesig"
    This reverts commit 24fe7cd3ccc40f70e521f86975b2ab8e4d92277d.

--- widget/nsNativeTheme.cpp
+++ widget/nsNativeTheme.cpp
@@ -584,6 +584,7 @@ bool nsNativeTheme::IsWidgetAlwaysNonNative(nsIFrame* aFrame,
                                             StyleAppearance aAppearance) {
   return IsWidgetScrollbarPart(aAppearance) ||
          aAppearance == StyleAppearance::FocusOutline ||
+         aAppearance == StyleAppearance::Menuarrow ||
          aAppearance == StyleAppearance::Toolbarbutton ||
          aAppearance == StyleAppearance::ProgressBar ||
          aAppearance == StyleAppearance::Progresschunk ||
